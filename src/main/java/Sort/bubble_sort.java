670179a3d024f148e835dfbf9aaea0c11571872d
10080115 <1103438014@qq.com>
2016-06-14 16:34:54 +0800

    2016-06-14 16:34

---
+++ src/main/java/Sort/bubble_sort.java
@@ -0,0 +1,15 @@
+package Sort;
+
+/**
+ * Created by lishiwei on 16/6/14.
+ */
+
+//冒泡排序
+
+public class bubble_sort {
+    // {3, 5, 7, 9, 2, 4, 8, 10, 1, 2};
+    public static void main(String[] args) {
+        int[] arr = constant.arr;
+        bubble_sort(arr);
+    }
+}
---
+++ src/main/java/Sort/constant.java
@@ -0,0 +1,8 @@
+package Sort;
+
+/**
+ * Created by lishiwei on 16/6/14.
+ */
+public class constant {
+    public static final int[] arr = {3, 5, 7, 9, 2, 4, 8, 10, 1, 2};
+}
